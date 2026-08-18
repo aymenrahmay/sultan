8557b3b8fd62f9df7f222ed2231815d917340f95
aymenrahmay <HALOUFA1988@git>
2026-08-19 00:29:35 +0200

    Disable quick creaition

--- sultan_stock/models/stock_picking.py
+++ sultan_stock/models/stock_picking.py
@@ -1,5 +1,43 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+
+    block_as_stock_destination = fields.Boolean(
+        string="Block as Stock Destination",
+        help=(
+            "Prevent stock operations from placing products directly in this "
+            "location. The location can still be used as a source location."
+        ),
+    )
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _action_done(self, cancel_backorder=False):
+        blocked_moves = self.filtered(
+            lambda move: move.state not in ("done", "cancel")
+            and move.location_dest_id.block_as_stock_destination
+            and move.location_dest_id.usage == "internal"
+            and move.location_id != move.location_dest_id
+            and (move.quantity > 0 or move.is_inventory)
+        )
+        if blocked_moves:
+            locations = ", ".join(
+                sorted(set(blocked_moves.mapped("location_dest_id.complete_name")))
+            )
+            raise UserError(_(
+                "Products cannot be received directly into the following main "
+                "stock location(s): %(locations)s. Select a child/bin location "
+                "instead.",
+                locations=locations,
+            ))
+
+        return super()._action_done(cancel_backorder=cancel_backorder)
 
 
 class StockPicking(models.Model):
